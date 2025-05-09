692477037682688739eef948009746ef28727797
samir khanal <52138139+samirkhanalofficial@users.noreply.github.com>
2025-05-09 06:51:59 +0545

    fix: firebase.json file updated for using render's secret file path
    The file is uploaded via render and is now secured. since older credientials have been revoked.

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 
 const initializeFirebaseApp = async () => {
   admin.initializeApp({
-    credential: admin.credential.cert("firebase.json"), // Replace with your service account key
+    credential: admin.credential.cert("/etc/secrets/firebase.json"), // Replace with your service account key
   });
 };
 
